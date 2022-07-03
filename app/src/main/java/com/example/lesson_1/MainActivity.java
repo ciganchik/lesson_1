5f2625b05127e5cbfece1dc9ba4ee05d01c77d3f
cigan4ik <108021555+cigan4ik@users.noreply.github.com>
2022-07-03 17:55:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson_1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.lesson_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+    }
+
+    public void openEdit(View view){
+        Intent intent = new Intent(this, edit_activity.class);
+        startActivity(intent);
+    }
+}
